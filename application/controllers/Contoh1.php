2fd3c3d766f0957b499b525f4c05e85370acb141
BarrenLand <danielkevinlangley@live.com>
2021-09-17 11:29:34 +0900

    Tugas pertemuan 1 dan contoh 1 revisi

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -1,12 +1,11 @@
 <?php
-class Contohl extends CI_Controller
-{
-    public function index()
+class Contoh1 extends CI_Controller {
+
+    Public function index()
     {
-        echo "<hl>Perkenalkan</h1>”;
-        echo"Nama saya Imam Nawawi
-            Saya tinggal di daerah Ciputat
-            olahraga yang saya sukai adalah
-            Bulutangkis";
+            echo "<h1>Perkenalkan</h1>";
+            echo "Nama saya Imam Nawawi
+                        Saya tinggal di daerah Ciputat
+                        Olahraga yang saya sukai adalah";
     }
 }
