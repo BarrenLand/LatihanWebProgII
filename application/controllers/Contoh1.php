e711adf3a5c9d2244be9d99a283ac15b5c4cb631
BarrenLand <danielkevinlangley@live.com>
2021-09-17 11:11:16 +0900

    Tugas Pertemuan 1 & Contoh 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contohl extends CI_Controller
+{
+    public function index()
+    {
+        echo "<hl>Perkenalkan</h1>”;
+        echo"Nama saya Imam Nawawi
+            Saya tinggal di daerah Ciputat
+            olahraga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
